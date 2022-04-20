800c83d1777f2a0baf14d1ba108aa080f774c01a
sirSantana23 <salazarmiguelito23gmail.com>
2022-04-19 20:19:18 -0500

    themematerialui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({ 
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#1b333d",
+      dark: "#173947",
+      contrastText: "#f1f1f1",
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f50057',
+      dark: '#c51162',
+      contrastText: '#f1f1f1',
+    },
+  },
+});
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+  <Component {...pageProps} />
+    </ThemeProvider>
+  )
+  
 }
 
 export default MyApp
